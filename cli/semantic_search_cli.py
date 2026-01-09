1ab7216ca4453a009067d51682641c95d302ffee
bremsstrahlung-57 <sagar292905@gmail.com>
2026-01-09 14:12:00 +0530

    semantic chunking

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import re
 
 from lib.semantic_search import (
     SemanticSearch,
@@ -34,17 +35,59 @@ def main():
         help="Number of results from Semantic search",
     )
 
+    chunk_parser = subparsers.add_parser(
+        "chunk", help="Chunking to split long text into smaller pieces for embedding."
+    )
+    chunk_parser.add_argument("text", type=str, help="Text to chunk")
+    chunk_parser.add_argument(
+        "chunk_size",
+        type=int,
+        nargs="?",
+        default=200,
+        help="Size of chunk (default=200)",
+    )
+    chunk_parser.add_argument(
+        "overlap",
+        type=int,
+        nargs="?",
+        default=0,
+        help="Overlap chunks",
+    )
+
+    semantic_chunk_parser = subparsers.add_parser(
+        "semantic_chunk", help="Semantic chunking"
+    )
+    semantic_chunk_parser.add_argument("text", type=str, help="Text to chunk")
+    semantic_chunk_parser.add_argument(
+        "max_chunk_size",
+        type=int,
+        nargs="?",
+        default=4,
+        help="Size of chunk (default=4)",
+    )
+    semantic_chunk_parser.add_argument(
+        "overlap",
+        type=int,
+        nargs="?",
+        default=0,
+        help="Overlap chunks",
+    )
+
     args = parser.parse_args()
 
     match args.command:
         case "verify_model":
             verify_model()
+
         case "verify_embeddings":
             verify_embeddings()
+
         case "embed_text":
             embed_text(args.text)
+
         case "embedquery":
             embed_query_text(args.query)
+
         case "search":
             search_s = SemanticSearch()
             with open("data/movies.json", "r") as file:
@@ -57,6 +100,38 @@ def main():
                     f"{i}. {movie['title']}: (score: {score:.4f})\n{movie['description'][:100]}..."
                 )
                 print()
+
+        case "chunk":
+            words = args.text.split()
+            chunks = []
+
+            if args.overlap < 0:
+                args.overlap = 0
+            if len(words) < args.overlap:
+                args.overlap = len(words)
+
+            for i in range(0, len(words), args.chunk_size - args.overlap):
+                chunk = " ".join(words[i : i + args.chunk_size])
+                chunks.append(chunk)
+
+            print(f"Chunking {len(args.text)} characters")
+            for idx, chunk in enumerate(chunks, start=1):
+                print(f"{idx}. {chunk}")
+
+        case "semantic_chunk":
+            sentences = re.split(r"(?<=[.!?])\s+", args.text)
+            chunks = []
+            step = args.max_chunk_size - args.overlap
+            i = 0
+
+            while i + args.max_chunk_size <= len(sentences):
+                chunk = " ".join(sentences[i : i + args.max_chunk_size])
+                chunks.append(chunk)
+                i += step
+
+            print(f"Semantically chunking {len(args.text)} characters")
+            for i, sentence in enumerate(chunks, start=1):
+                print(f"{i}. {sentence}")
 
         case _:
             parser.print_help()
